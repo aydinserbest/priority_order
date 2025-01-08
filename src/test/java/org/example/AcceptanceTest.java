c2a6a203e0e260889cb588335148c680082335b7
aydinserbest <101752975+aydinserbest@users.noreply.github.com>
2025-01-08 01:55:58 +0100

    Update tag filters for testing scenarios

--- src/test/java/org/example/AcceptanceTest.java
+++ src/test/java/org/example/AcceptanceTest.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features",
-        tags = "@runnerClassTag"
+        features = "src/test/resources/features"
+        //tags = "@runnerClassTag"
 )
 public class AcceptanceTest {
 
